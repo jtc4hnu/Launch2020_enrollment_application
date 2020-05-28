cc476c8731f0ff9c39dc78f06b72027bb3b7304e
Jack Chandler <jtc4hnu@virginia.edu>
2020-05-28 16:21:53 -0400

    Create new Users

--- src/dataDisplay.js
+++ src/dataDisplay.js
@@ -85,136 +85,52 @@ class DataDisplay extends Component {
 
     CreateTeacher = (name, email) => {
         this.props.auth.createUserWithEmailAndPassword(
-            document.getElementById("inputEmail").value,
+            email,
             process.env.REACT_APP_DEFAULT_PASSWORD
         )
             .then(apicall => {
                 this.props.database.collection("Privileges").doc(apicall.user.uid).set({
                     privilege: "TEACHER"
                 })
+
+                this.props.database.collection("Teachers").doc(apicall.user.uid).set({
+                    name: name,
+                    email: email,
+                    classes: []
+                })
             })
         this.GetCollections();
     }
 
     CreateStudent = (name, email) => {
+        this.props.auth.createUserWithEmailAndPassword(
+            email,
+            process.env.REACT_APP_DEFAULT_PASSWORD
+        )
+            .then(apicall => {
+                this.props.database.collection("Privileges").doc(apicall.user.uid).set({
+                    privilege: "STUDENT"
+                })
 
+                this.props.database.collection("Students").doc(apicall.user.uid).set({
+                    name: name,
+                    email: email,
+                    classes: []
+                })
+            })
     }
 
     render() {
         return (
             <div>
-                <h3>Teachers</h3>
-                <DataTable CreateUser={this.CreateTeacher} privilege={this.state.privilege} users={this.state.teachers} />
-                <h3>Students</h3>
-                <DataTable CreateUser={this.CreateStudent} privilege={this.state.privilege} users={this.state.students} />
-
-
-
-                {/* <table className="Teachers">
-                    <thead>
-                        <tr>
-                            <th>Teacher Name</th>
-                            <th>Teacher Email</th>
-                            <th>Teacher ID</th>
-                            <th colSpan="6">Classes</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.teachers.map(teacher => {
-                                return (
-                                    <tr>
-                                        <td>{teacher.name}</td>
-                                        <td>{teacher.email}</td>
-                                        <td>{teacher.id}</td>
-                                        {
-                                            teacher.classes.map(course => {
-                                                return <td><button>{course}</button></td>
-                                            })
-                                        }
-                                        <td><button>Add Course</button></td>
-                                    </tr>
-                                )
-                            })
-                        }
-                        {
-                            this.state.addingTeacher ?
-                                <tr>
-                                    <td><input id="NewTeacherName" placeholder="Teacher Name" /></td>
-                                    <td><input id="NewTeacherEmail" placeholder="Teacher Email" /></td>
-                                    <td><button onClick={() => {
-                                        this.props.auth.createUserWithEmailAndPassword(
-                                            document.getElementById("inputEmail").value,
-                                            process.env.REACT_APP_DEFAULT_PASSWORD
-                                        )
-                                            .then(apicall => {
-                                                this.props.database.collection("Privileges").doc(apicall.user.uid).set({
-                                                    privilege: "TEACHER"
-                                                })
-                                            })
-                                        this.GetCollections();
-                                    }}>Create Teacher</button></td>
-                                </tr>
-                                :
-                                this.state.privilege === "ADMIN" &&
-                                <tr>
-                                    <td><button onClick={() => this.setState({
-                                        addingTeacher: true
-                                    })}>Add Teacher</button></td>
-                                </tr>
-                        }
-                    </tbody>
-                </table>
-                <table className="Students">
-                    <thead>
-                        <tr>
-                            <th>Student Name</th>
-                            <th>Student Email</th>
-                            <th>Student ID</th>
-                            <th colSpan="6">Classes</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.students.map(student => {
-                                return (
-                                    <tr>
-                                        <td>{student.name}</td>
-                                        <td>{student.email}</td>
-                                        <td>{student.id}</td>
-                                    </tr>
-                                )
-                            })
-                        }
-                        {
-                            this.state.addingStudent ?
-                                <tr>
-                                    <td><input placeholder="Student Name" /></td>
-                                    <td><input placeholder="Student Email" /></td>
-                                    <td><button onClick={() => {
-                                        this.props.auth.createUserWithEmailAndPassword(
-                                            document.getElementById("inputEmail").value,
-                                            process.env.REACT_APP_DEFAULT_PASSWORD
-                                        )
-                                            .then(apicall => {
-                                                this.props.database.collection("Privileges").doc(apicall.user.uid).set({
-                                                    privilege: "STUDENT"
-                                                })
-                                            })
-                                        this.GetCollections();
-                                    }}>Create Student</button></td>
-                                </tr>
-                                :
-                                (this.state.privilege === "TEACHER" ||
-                                    this.state.privilege === "ADMIN") &&
-                                <tr>
-                                    <td><button onClick={() => this.setState({
-                                        addingStudent: true
-                                    })}>Add Student</button></td>
-                                </tr>
-                        }
-                    </tbody>
-                </table> */}
+                <div>
+                    <h3>Teachers</h3>
+                    <DataTable CreateUser={this.CreateTeacher} privilege={this.state.privilege} users={this.state.teachers} />
+                </div>
+                <div>
+                    <h3>Students</h3>
+                    <DataTable CreateUser={this.CreateStudent} privilege={this.state.privilege} users={this.state.students} />
+                </div>
 
                 <button onClick={() =>
                     this.props.auth.signOut()
